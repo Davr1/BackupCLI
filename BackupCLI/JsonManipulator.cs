4ad3d7201ea53aa60071978c6a06018084789bd1
Davr1 <42148912+Davr1@users.noreply.github.com>
2023-11-24 18:45:19 +0100

    Json recursive validation

--- BackupCLI/JsonManipulator.cs
+++ BackupCLI/JsonManipulator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Quartz;
@@ -18,10 +19,10 @@ public static class JsonManipulator
         if (JsonSerializer.Deserialize<List<BackupJob>>(File.ReadAllText(path), Options) is not { } backupJobs)
             throw new JsonException("Input file is not a valid JSON file.");
 
-        backupJobs.ForEach(job => job.Validate());
-
         foreach (var job in backupJobs)
         {
+            job.Validate();
+
             foreach (var s in job.Sources.Where(source => !Path.Exists(source)))
                 throw new DirectoryNotFoundException($"Source directory {s} does not exist.");
         }
@@ -33,8 +34,12 @@ public static class JsonManipulator
 public class BackupJob : ValidJson
 {
     public List<string> Sources { get; set; } = new();
+    
     public List<string> Targets { get; set; } = new();
+
+    [JsonIgnore]
     public CronExpression? Cron { get; set; }
+
     public string? Timing
     {
         get => Cron?.CronExpressionString;
@@ -44,22 +49,28 @@ public class BackupJob : ValidJson
 
             List<string> parts = value.Split(' ').ToList();
 
+            // standard cron expression are incompatible with the quartz format, so we need to convert them
             if (parts.Count == 5)
             {
+                // seconds
                 parts.Insert(0, "0");
+                // day of week and day of month are mutually exclusive
                 if (parts[3] != "?" && parts[5] != "?") parts[5] = "?";
             }
+
             Cron = new CronExpression(string.Join(' ', parts));
         }
     }
 
     public BackupRetention Retention { get; set; } = new();
+
     public BackupMethod Method { get; set; } = BackupMethod.Full;
 }
 
 public class BackupRetention : ValidJson
 {
     public int Count { get; set; } = 2;
+
     public int Size { get; set; } = 0;
 }
 
@@ -71,6 +82,9 @@ public enum BackupMethod
     Incremental
 }
 
+/// <summary>
+/// Provides non-null type checking and default value support for deserialization using the default JSON library. 
+/// </summary>
 public class ValidJson
 {
     private Dictionary<string, object?> DefaultProps { get; }
@@ -79,8 +93,14 @@ public class ValidJson
 
     public void Validate()
     {
-        foreach (var prop in GetType().GetProperties())
+        PropertyInfo[] props = GetType().GetProperties();
+        MethodInfo validate = GetType().GetMethod("Validate")!;
+
+        foreach (var prop in props.Where(p => p.GetCustomAttribute<JsonIgnoreAttribute>() is null))
             prop.SetValue(this, PropOrDefault(prop) ?? throw new JsonException($"{prop.Name} cannot be null."));
+
+        foreach (var prop in props.Where(p => typeof(ValidJson).IsAssignableFrom(p.PropertyType)))
+            validate.Invoke(prop.GetValue(this), null);
     }
 
     protected ValidJson()
